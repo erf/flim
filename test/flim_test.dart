315ed36ccf143b3af91f79b62e22abc504077861
Erlend Lind Madsen <erlendf80@gmail.com>
2021-03-12 16:06:27 +0100

    cleanup unused tests

--- test/flim_test.dart
+++ test/flim_test.dart
@@ -1,9 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flim/flim.dart';
-
 void main() {
-  test('one plus one is two', () {
-    expect(1 + 1, 2);
-  });
+  test('TODO', () {});
 }
